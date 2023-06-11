db00b3adb7a211723f03f986bae8487bd6da8c59
Sabid Hasan <sabid.soft@gmail.com>
2023-06-11 10:13:19 +0600

    server.js file is modified

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const supplierRoute = require("./routes/supplier.route");
 const stockRoute = require("./routes/stock.route");
 const userRoute = require("./routes/user.route");
 
-// middlewares
+// application level middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
     })
 })
 
-// routes
+// router level middlewares
 app.use("/api/v1/product", productRoute);
 app.use("/api/v1/brand", brandRoute);
 app.use("/api/v1/store", storeRoute);
@@ -39,7 +39,7 @@ app.use("/api/v1/user", userRoute);
 mongoose.connect(process.env.DATABASE)
     .then(() => { console.log(`Database connection is successful`) })
 
-// server
+// listening server
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Server is running on port ${PORT}`)
 })
